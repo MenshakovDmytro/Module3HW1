096cdab59d22ed7e2845a2db291e3a6a407d2545
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-06 17:03:27 +0200

    added StringComparer

---
+++ Module3HW1/Module3HW1/Comparers/StringComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace Module3HW1.Comparers
+{
+    public class StringComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var firstItem = x as string;
+            var secondItem = y as string;
+            return firstItem.CompareTo(secondItem);
+        }
+    }
+}
