5ff97dde038f5c9de437910ad3906ee99920ff2a
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-06 17:03:19 +0200

    added IntComparer

---
+++ Module3HW1/Module3HW1/Comparers/IntComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace Module3HW1.Comparers
+{
+    public class IntComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var firstItem = (int)x;
+            var secondItem = (int)y;
+            return firstItem.CompareTo(secondItem);
+        }
+    }
+}
